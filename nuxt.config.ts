77b80394a2623a35b2e8850d6ab7da52109db2d9
narr07 <dinarpermadi07@gmail.com>
2024-07-06 14:52:07 +0700

    chore: Update app.vue and nuxt.config.ts, add dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/image']
+  modules: [
+    '@nuxt/image',
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/fonts',
+    'nuxt-swiper',
+    '@nuxthq/studio'
+  ]
 })
---
+++ plugins/aos.client.ts
@@ -0,0 +1,8 @@
+// plugins/aos.client.ts
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+export default defineNuxtPlugin((nuxtApp) => {
+    nuxtApp.AOS = new AOS.init({});
+});
